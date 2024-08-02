3b24bf5e9509e13c09d68c82b13d072b62f6563d
ongthien <ongthiendev@gmail.com>
2024-08-02 17:46:36 +0700

    finished phonebook step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import NewContact from "./components/NewContact";
 import Contacts from "./components/Contacts";
 import Filter from "./components/Filter";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [phoneNum, setPhoneNum] = useState("");
   const [filteredPersons, setFilteredPersons] = useState([]);
   const [showAll, setShowAll] = useState(true);
+
+  const dbUrl = "http://localhost:3001/persons";
+  useEffect(() => {
+    axios.get(dbUrl).then((res) => {
+      // console.log(res);
+      setPersons(res.data);
+    });
+  }, []);
+
   const handleAddName = (event) => {
     event.preventDefault();
     if (persons.find((p) => p.name === newName)) {
