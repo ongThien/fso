615f313c1f7562c0c0a27ea1f7acbbb92bc31109
ongthien <ongthiendev@gmail.com>
2024-09-30 03:06:15 +0700

    finished 5.4: Blog List Frontend, step 4

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
-import blogService from "./services/blogs";
 import LoginForm from "./components/LogInForm";
 import BlogForm from "./components/BlogForm";
 import Notification from "./components/Notification";
+import blogService from "./services/blogs";
 import loginService from "./services/login";
 
 const App = () => {
@@ -12,12 +12,23 @@ const App = () => {
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
   const [message, setMessage] = useState(null);
+  const [isError, setIsError] = useState(false);
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [url, setUrl] = useState("");
 
+  // some utils variable and function
   const localStorageUserKey = "loggedBlogAppUser";
+  const showMessage = (message, isError) => {
+    setMessage(message);
+    setIsError(isError);
+    setTimeout(() => {
+      setMessage(null);
+      setIsError(false);
+    }, 5000);
+  };
 
+  //fetch all blogs from db
   useEffect(() => {
     const fetchData = async () => {
       const blogs = await blogService.getAll();
@@ -27,6 +38,7 @@ const App = () => {
     fetchData();
   }, []);
 
+  // read user token if there is one
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem(localStorageUserKey);
     if (loggedUserJSON) {
@@ -44,17 +56,15 @@ const App = () => {
         username,
         password,
       });
-
+      
       window.localStorage.setItem(localStorageUserKey, JSON.stringify(user));
       blogService.setToken(user.token);
       setUser(user);
       setUsername("");
       setPassword("");
+      showMessage(`Welcome back, ${user.username}`, false);
     } catch (exception) {
-      setMessage("Wrong credentials");
-      setTimeout(() => {
-        setMessage(null);
-      }, 5000);
+      showMessage("Wrong username or password", true);
     }
   };
 
@@ -62,20 +72,14 @@ const App = () => {
     window.localStorage.removeItem(localStorageUserKey);
     blogService.setToken("");
     setUser(null);
-    setMessage("user logged out");
-    setTimeout(() => {
-      setMessage(null);
-    }, 5000);
+    showMessage("User logged out", false);
   };
 
   const addBlog = async (event) => {
     event.preventDefault();
 
     if (!(title && url)) {
-      setMessage("title & url required!");
-      setTimeout(() => {
-        setMessage(null);
-      }, 5000);
+      showMessage("title & url required!", true);
       return;
     }
 
@@ -85,13 +89,14 @@ const App = () => {
     setAuthor("");
     setUrl("");
     setBlogs(blogs.concat(blog));
+    showMessage(`a new blog ${blog.title} by ${blog.author} added`, false);
   };
 
   return (
     <div>
       <h2>{user ? "blogs" : "log in to application"}</h2>
 
-      <Notification message={message} />
+      <Notification message={message} isError={isError} />
 
       {user ? (
         <div>
--- part5/src/components/Notification.jsx
+++ part5/src/components/Notification.jsx
@@ -1,9 +1,27 @@
-const Notification = ({ message }) => {
+const Notification = ({ message, isError }) => {
   if (!message) {
     return <></>
   }
 
-  console.log(message);
+  const successNoti = {
+    color: "green",
+    backGround: "white",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    borderColor: "green",
+    padding: 10,
+    marginBottom: 10,
+  };
+
+  const errorNoti = {
+    ...successNoti,
+    color: "red",
+    backGround: "lightgrey",
+    borderColor: "red",
+  };
+
+  return <div style={isError ? errorNoti : successNoti}>{message}</div>;
 };
 
 export default Notification;
