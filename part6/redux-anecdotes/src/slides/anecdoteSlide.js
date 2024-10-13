45bbb04798563ce6ac4ac592f10418f1353be0bf
ongthien <ongthiendev@gmail.com>
2024-10-14 00:45:54 +0700

    added example when using createAsyncThunk for learning purpose

--- part6/redux-anecdotes/src/slides/anecdoteSlide.js
+++ part6/redux-anecdotes/src/slides/anecdoteSlide.js
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import anecdoteService from "../services/anecdotes";
 
+// Using manually created thunks
+
 const anecdoteSlide = createSlice({
   name: "anecdotes",
   initialState: [],
@@ -37,3 +39,59 @@ export const voteAnecdote = (anecdote) => async (dispatch) => {
 
 export const { create, vote, set } = anecdoteSlide.actions;
 export default anecdoteSlide.reducer;
+
+// Using createAsyncThunk
+
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import anecdoteService from "../services/anecdotes";
+
+// export const initializeAnecdotes = createAsyncThunk(
+//   'anecdotes/fetchAll',
+//   async () => {
+//     const anecdotes = await anecdoteService.getAll();
+//     return anecdotes;
+//   }
+// );
+
+// export const createAnecdote = createAsyncThunk(
+//   'anecdotes/create',
+//   async (anecdote) => {
+//     const newAnecdote = await anecdoteService.create({
+//       content: anecdote,
+//       votes: 0,
+//     });
+//     return newAnecdote;
+//   }
+// );
+
+// export const voteAnecdote = createAsyncThunk(
+//   'anecdotes/vote',
+//   async (anecdote) => {
+//     const updatedAnecdote = await anecdoteService.update(anecdote.id, {
+//       ...anecdote,
+//       votes: anecdote.votes + 1,
+//     });
+//     return updatedAnecdote;
+//   }
+// );
+
+// const anecdoteSlice = createSlice({
+//   name: 'anecdotes',
+//   initialState: [],
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(initializeAnecdotes.fulfilled, (state, action) => {
+//         return action.payload;
+//       })
+//       .addCase(createAnecdote.fulfilled, (state, action) => {
+//         state.push(action.payload);
+//       })
+//       .addCase(voteAnecdote.fulfilled, (state, action) => {
+//         const upvoted = action.payload;
+//         return state.map((anec) => (anec.id === upvoted.id ? upvoted : anec));
+//       });
+//   },
+// });
+
+// export default anecdoteSlide.reducer;
