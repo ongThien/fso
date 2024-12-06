43f6618d91305cd3c6662f2817a4c38652fdb2fb
ongThien <ongthiendev@gmail.com>
2024-12-06 10:46:14 +0700

    finished Exercise 10.12: environment variables

--- part10/rate-repository-app/app/src/utils/apolloClient.js
+++ part10/rate-repository-app/app/src/utils/apolloClient.js
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-
+import Constants from "expo-constants";
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://192.168.1.11:4000/graphql',
+    uri: Constants.expoConfig.extra.apolloURI,
     cache: new InMemoryCache(),
   });
 };
